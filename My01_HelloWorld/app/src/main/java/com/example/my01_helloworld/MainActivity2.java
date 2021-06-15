654194c340c5e702548b897d64ce2e9cfee5c104
301-18\SEC <0000>
2021-06-15 12:37:04 +0900

    Main에 int a = 0을 추가

--- My01_HelloWorld/app/src/main/java/com/example/my01_helloworld/MainActivity2.java
+++ My01_HelloWorld/app/src/main/java/com/example/my01_helloworld/MainActivity2.java
@@ -14,6 +14,7 @@ public class MainActivity2 extends AppCompatActivity {
     //소스 작성은 onCreate에서 한다.
     //AppConpatActivity를 상속 받았다.
     EditText edtNum;
+	int a = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
