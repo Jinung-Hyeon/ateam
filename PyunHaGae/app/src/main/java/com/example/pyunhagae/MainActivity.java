5e25cb160a9b81cc5e499e421ba994ccc40e4313
301-18\SEC <0000>
2021-06-16 11:37:44 +0900

    테스트

--- PyunHaGae/app/src/main/java/com/example/pyunhagae/MainActivity.java
+++ PyunHaGae/app/src/main/java/com/example/pyunhagae/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String a = "개 ㅈ같네";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
