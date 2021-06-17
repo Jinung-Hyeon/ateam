0f258da0244f5c4a0b79cb038bd51524052f4530
Jinung-Hyeon <oiuoiyu@gmail.com>
2021-06-17 12:48:08 +0900

    로그인 후 화면 변수 추가했습니다.

--- PyunHaGae/app/src/main/java/com/example/pyunhagae/DeviceEnrolFrag.java
+++ PyunHaGae/app/src/main/java/com/example/pyunhagae/DeviceEnrolFrag.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DeviceEnrolFrag extends Fragment {
+    Button btn_iot, btn_phone, btn_link, btn_wifi;  //기기등록 프래그먼트에서 사용되는 변수
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_device_enrol_frag,container,false);
+        btn_iot = rootView.findViewById(R.id.btn_iot);
+        btn_phone = rootView.findViewById(R.id.btn_phone);
+        btn_link = rootView.findViewById(R.id.btn_link);
+        btn_wifi = rootView.findViewById(R.id.btn_wifi);
+
         return rootView;
     }
 }
--- PyunHaGae/app/src/main/java/com/example/pyunhagae/NoDeviceFrag.java
+++ PyunHaGae/app/src/main/java/com/example/pyunhagae/NoDeviceFrag.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class NoDeviceFrag extends Fragment {
+    Button doEnrolDevice;   //기기동록버튼 변수
+
+    ImageView noDevice_frag_iv; //기기등록안된 프래그먼트에 있는 이미지뷰 변수
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_no_device_frag,container,false);
+        doEnrolDevice = rootView.findViewById(R.id.btn_doEnrolDevice);
+        noDevice_frag_iv = rootView.findViewById(R.id.noDevice_frag_iv);
+
         return rootView;
     }
 }
